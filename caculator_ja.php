6da68ced084b43b9261f7cc2bcf8c9c6aec53c16
Pxxo.K <69616164+Pxxo@users.noreply.github.com>
2021-03-15 21:38:20 +0900

    Update caculator_ja.php

--- caculator_ja.php
+++ caculator_ja.php
@@ -6,6 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="css/caculator_ja.css">
+  <link rel="icon" href="img/favicon.ico">
   <title>原神 樹脂 計算機</title>
 </head>
 
