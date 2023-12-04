12ea0313b60bb3d981b99745688b7e3e9f83717b
Pino0202 <pinochi0202@gmail.com>
2023-12-05 00:35:21 +0900

    デプロイ調整

--- caculator_ja.php
+++ caculator_ja.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/caculator_ja.css">
     <link rel="icon" href="img/favicon.ico">
-    <title>原神 樹脂 計算機</title>
+    <title>原神 樹脂計算機</title>
 </head>
 
 <body>
--- index.php
+++
@@ -1,2 +0,0 @@
-<?php
-header("Location: caculator_ja.php");
